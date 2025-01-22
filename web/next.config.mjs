b214c7685b3d994ca4557395a956619524b9e1e3
C N <147255377+Candra0x6@users.noreply.github.com>
2025-01-23 00:33:38 +0700

    Update configuration and dependencies for improved functionality

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https', // Allow HTTPS images
+          hostname: '**', // Allow all domains
+        },
+        {
+          protocol: 'http', // Allow HTTP images (if needed)
+          hostname: '**', // Allow all domains
+        },
+      ],
+    },
+  };
+  
 
 export default nextConfig;
