33a731a10190f8042c3c62b02c42375af43c9c21
C N <147255377+Candra0x6@users.noreply.github.com>
2025-01-23 00:49:41 +0700

    Enhance dashboard layout by integrating MultiSidebarProvider and SidebarChat for improved user experience

--- web/app/dashboard/layout.tsx
+++ web/app/dashboard/layout.tsx
@@ -1,16 +1,22 @@
-import { AppSidebar } from "@/components/elements/Sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import SidebarChat from '@/components/editor/elements/ai-chat/SidebarChat';
+import { AppSidebar } from '@/components/elements/Sidebar';
+import {
+  MultiSidebarProvider,
+  SidebarInset,
+} from '@/components/ui/multisidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="flex mx-auto max-h-screen overflow-hidden w-full pt-5">
-        <div className="pl-2">
-          <SidebarTrigger />
-        </div>
-        {children}
-      </main>
+    <SidebarProvider name="kon">
+      <MultiSidebarProvider>
+        <AppSidebar />
+
+        <main className="flex max-h-screen overflow-hidden w-full pt-5">
+          <SidebarInset>{children}</SidebarInset>
+        </main>
+        <SidebarChat />
+      </MultiSidebarProvider>
     </SidebarProvider>
   );
 }
