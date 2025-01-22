1f839781b1929254d8b817a485d32989b01f745b
C N <147255377+Candra0x6@users.noreply.github.com>
2025-01-23 00:50:27 +0700

    Add SidebarChat component for AI chat integration

---
+++ web/components/editor/elements/ai-chat/SidebarChat.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { SidebarChatWrapper } from './SidebarChatWrapper';
+import { RiQuillPenAiFill } from 'react-icons/ri';
+
+const SidebarChat: React.FunctionComponent = (props) => {
+  return <SidebarChatWrapper />;
+};
+
+export default SidebarChat;
